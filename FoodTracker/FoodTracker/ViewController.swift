07fef008a21dfcdf39be1de67c5c1add6a82b0d1
Laura Machado <lauramachado@Lauras-MacBook-Pro.local>
2015-10-13 01:30:35 -0400

    r

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Laura Machado on 2015-10-11.
+//  Copyright (c) 2015 Laura Machado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
